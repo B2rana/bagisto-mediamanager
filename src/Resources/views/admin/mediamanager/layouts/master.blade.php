8d11429db5d521aff67cdb66875ce6fc7127f0b9
Pushkar Rana <pushkar_rana2005@yahoo.co.in>
2022-03-01 12:32:20 +0530

    fix html issue

--- src/Resources/views/admin/mediamanager/layouts/master.blade.php
+++ src/Resources/views/admin/mediamanager/layouts/master.blade.php
@@ -1,15 +1,5 @@
 @extends('admin::layouts.master')
 
 @section('content-wrapper')
-    <div class="inner-section">
-    
-        @include ('admin::layouts.nav-aside')
-
-        <div class="content-wrapper">
-
-            @yield('content')
-
-        </div>
-        
-    </div>
+    @yield('content')
 @stop
